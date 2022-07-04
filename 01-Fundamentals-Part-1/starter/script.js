5f0be2ad6508bb45311c042e5094f0db3fafa7d4
Tejas Naik <RNTejas2005@gmail.com>
2022-07-04 18:26:16 +0530

    JavaScript Hello, World!

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,13 @@
-console.log("Hello");
+///////////////////////////////////
+// Hello World...
+///////////////////////////////////
+console.log("Hello, World");
+// alert("Hello, World!");
+let js = "amazing";
+console.log(js)
 
- 
+if (js == 'amazing') {
+    console.log("JavaScript is fun");
+}
+
+console.log(40 + 8 - 28 + 15);
