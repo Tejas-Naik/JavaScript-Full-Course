69a9afcd6d47db1d66624ccc2e07a8c2e4e9c7e2
Tejas Naik <RNTejas2005@gmail.com>
2022-07-06 15:05:52 +0530

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -224,6 +224,7 @@ if (birthYear >= 2000) {
 console.log(century);
 */
 
+/*
 ///////////////////////////////////
 // TYPE CONVERSION / COERcION
 ///////////////////////////////////
@@ -246,3 +247,37 @@ console.log('23' / '10');       // *  will convert to numbers
 
 console.log('23' + '10' - 3);
 
+*/
+
+
+///////////////////////////////////
+// TRUTHY AND FALSY VALUES
+///////////////////////////////////
+// falsy values are not falsy but they become falsy when we try to convert them to boolean
+
+// 5 FALSY VALUES : NULL, UNDEFINED, 0, "", NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+console.log(Boolean("Tejas"));
+console.log(Boolean({}));
+
+// implicitly javascript converting to boolean
+const money = 0;
+if (money) {
+    console.log("Don't spend it all!");
+} else {
+    console.log("You should get a job!");
+}
+
+
+let height;
+if (height) {
+    console.log("Height is defined!");
+}
+else {
+    console.log("Height is UNDEFINED!")
+}
