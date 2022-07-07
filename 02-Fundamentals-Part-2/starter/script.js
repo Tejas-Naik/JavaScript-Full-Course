855592b17a7df78dda1e9ad89fae200211dcaf78
Tejas Naik <RNTejas2005@gmail.com>
2022-07-07 12:24:50 +0530

    Introduction to functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,32 @@ if (hasDriversLicence) console.log("I can drive...");
 // const private = 543;
 
 */
+
+///////////////////////////////
+// FUNCTIONS
+///////////////////////////////
+// Function is also a variable but for multiple lines of code
+function logger() {
+    console.log("My name is Tejas.");
+}
+
+// Invoking / calling / running a function
+logger();
+logger();
+logger();
+
+// Functions with inputs
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    // returns the output so we can store into variable or use other operators
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
