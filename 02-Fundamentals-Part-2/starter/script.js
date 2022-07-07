b67c9165872266346a43c635093a391a847ddff9
Tejas Naik <RNTejas2005@gmail.com>
2022-07-07 12:51:37 +0530

    Function Declarations & Function Expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,6 +12,8 @@ if (hasDriversLicence) console.log("I can drive...");
 
 */
 
+/*
+
 ///////////////////////////////
 // FUNCTIONS
 ///////////////////////////////
@@ -39,4 +41,28 @@ console.log(fruitProcessor(5, 0));
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
+*/
+
+///////////////////////////////
+// FUNCTIONS DECLARATION vs EXPRESSION
+///////////////////////////////
+// function declaration
+
+const age1 = calcAge1(2005);    // you can call declarations before intializing
+
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+}
+
+// const age1 = calcAge1(2005);
+console.log(age1)
+
+// function expression ==> produces value and stores to a variable
+// functions are values...
+const calcAge2 = function(birthYear) {
+    return 2022 - birthYear;
+}
+
+const age2 = calcAge2(2005);
+console.log(age2);
 
