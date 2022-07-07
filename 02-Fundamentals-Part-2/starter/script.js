2a7a627c505ffa7f9fa1bf2253c32ea16e2ebcac
Tejas Naik <RNTejas2005@gmail.com>
2022-07-07 11:52:36 +0530

    Strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+/*
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriverLicence = true;      
+if (hasDriversLicence) console.log("I can drive...");
+
+// const interface = "Audio";
+// const private = 543;
+
+*/
