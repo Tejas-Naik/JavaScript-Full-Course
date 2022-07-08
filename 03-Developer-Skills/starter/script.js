8c5c2855cb9edea563c0341d6edcf74b15e6c97f
Tejas Naik <RNTejas2005@gmail.com>
2022-07-08 23:07:16 +0530

    Developer setup and problem solving

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) console.log(23);
+
+/*
+to solve problems  always use 
+- Google
+- Stack Overflow
+- MDN Docs
+*/
+
+// Watch Lecture 59 again
