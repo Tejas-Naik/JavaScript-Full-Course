af4607280c3f94d9b34b34ce716fd95b183eb934
Tejas Naik <RNTejas2005@gmail.com>
2022-07-11 12:21:42 +0530

    Selecting Elements in DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,17 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+
+// Basics of selecting the elements using QuerySlector
+console.log(
+    document.querySelector('.message').textContent
+);
+
+// To change the value of an element
+document.querySelector('.message').textContent = "🎉 Correct Number";
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// To get the value of an element
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
 
