efbe377867fc89cc36367b0967f6c9f9ee15f9b4
Tejas Naik <RNTejas2005@gmail.com>
2022-07-11 14:03:13 +0530

    Implementing game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,16 +16,53 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const messageEl = document.querySelector('.message');
+const scoreEl = document.querySelector('.score');
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 // Reacting to the click events
 document.querySelector('.check').addEventListener('click',
     function () {
         let guess = Number(document.querySelector('.guess').value);
-        document.querySelector('.number').textContent = guess;
+        // document.querySelector('.number').textContent = guess;
         console.log(guess, typeof guess);
 
+        // No Input
         if (!guess) {
-            document.querySelector('.message').textContent = '⛔ NO Number!'
-        };
+            messageEl.textContent = '⛔ NO Number!'
+        }
+        // When player wins
+        else if (guess === secretNumber) {
+            messageEl.textContent = "🎉 Correct Number";
+
+            document.querySelector('body').style.backgroundColor = "#60b347";
+            document.querySelector('.number').style.width = "30rem"
+        }
+        // When player guesses higher
+        else if (guess > secretNumber) {
+            if (score > 1) {
+                messageEl.textContent = "🔻Guess Lower";
+                score--
+                scoreEl.textContent = score;
+            } else {
+                scoreEl.textContent = 0;
+                messageEl.textContent = '💥 You lost the game'
+            }
+        }
+        // When player guessed lower
+        else if (guess < secretNumber) {
+            if (score > 1) {
+                messageEl.textContent = "🔼 Guess Higher";
+                score--
+                scoreEl.textContent = score;
+            } else {
+                scoreEl.textContent = 0;
+                messageEl.textContent = '💥 You lost the game'
+            }
+        }
     }
 );
