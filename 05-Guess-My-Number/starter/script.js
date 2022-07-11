e17b38f3a669a314ee334148ce68ef2139862ef0
Tejas Naik <RNTejas2005@gmail.com>
2022-07-11 13:07:47 +0530

    Reacting to the events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 // Basics of selecting the elements using QuerySlector
 console.log(
     document.querySelector('.message').textContent
@@ -14,4 +14,18 @@ document.querySelector('.score').textContent = 10;
 // To get the value of an element
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
 
+
+// Reacting to the click events
+document.querySelector('.check').addEventListener('click',
+    function () {
+        let guess = Number(document.querySelector('.guess').value);
+        document.querySelector('.number').textContent = guess;
+        console.log(guess, typeof guess);
+
+        if (!guess) {
+            document.querySelector('.message').textContent = '⛔ NO Number!'
+        };
+    }
+);
