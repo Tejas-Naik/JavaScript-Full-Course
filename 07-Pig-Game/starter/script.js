6ecfb75b7e7ee55a223d1765f7f4e9507ecf71be
Tejas Naik <RNTejas2005@gmail.com>
2022-07-13 12:25:01 +0530

    Game Initial setup

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+// const scorePlayer0 = document.querySelector('#score--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
