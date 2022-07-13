5da67fe88ab7fea3829b758e0273281dac4eb84a
Tejas Naik <RNTejas2005@gmail.com>
2022-07-13 13:34:05 +0530

    Changing players functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,6 +2,8 @@
 
 // Selecting elements
 // const scorePlayer0 = document.querySelector('#score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,6 +17,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0]
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -28,9 +31,16 @@ btnRollEl.addEventListener('click', function () {
         // add dice to current score.
         currentScore += randomNumber;
         console.log(currentScore);
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // current0El.textContent = currentScore;
     } else {
         // switch next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
 
+        activePlayer = activePlayer == 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active')
     }
 });
+
