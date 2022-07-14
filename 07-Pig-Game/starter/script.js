71aca07e2cc6a3f8a4f8ba49b71a203c8bea513a
Tejas Naik <RNTejas2005@gmail.com>
2022-07-14 09:46:14 +0530

    Completed the PIG GAME

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,14 +13,28 @@ const btnRollEl = document.querySelector('.btn--roll');
 const btnNewEl = document.querySelector('.btn--new');
 const btnHoldEl = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0]
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.add('player--active')
+    player1El.classList.remove('player--active')
+    player0El.classList.remove('player--winner')
+    player1El.classList.remove('player--winner')
+
+}
+
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -70,6 +84,4 @@ btnHoldEl.addEventListener('click', function () {
     }
 });
 
-btnNewEl.addEventListener('click', function () {
-
-})
+btnNewEl.addEventListener('click', init);
