da3c1c09f8b01afd4c6cc56a726bfe28c0a875dc
Tejas Naik <RNTejas2005@gmail.com>
2022-07-14 10:13:08 +0530

    Overview of JavaScript

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Overview of JavaScript
+/*
+JavaScript is a High Level, Garbage-collected, Interpreted or just-in-time
+compiled, Multi Paradigm, Prototype based Object oriented programming languge 
+with First Class Function, Dynamically Typed, single threaded non-blocking 
+event loop
+*/
+
