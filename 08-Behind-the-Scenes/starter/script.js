8c5e83f34c74bedd75bea793424ce787eb1bd905
Tejas Naik <RNTejas2005@gmail.com>
2022-07-15 16:23:47 +0530

    Hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -20,11 +20,12 @@ Scoping controls how our program variables are organized and accessed.
 - Where do the variables live?
 
 Lexical Scoping
-- Scoping controlled by placement of functions and blocks in code; 
+- Scoping controlled by placement of functions and blocks in code;
 - Nested functions have access to the parent functions variables.
 - global variables are accessed by everywhere.
 */
 
+/*
 // Scoping in practice
 function calcAge(birthYear) {
     const age = 2022 - birthYear;
@@ -45,3 +46,62 @@ function calcAge(birthYear) {
 
 const firstName = "Tejas";
 calcAge(1986);
+
+*/
+
+// Variable Environment and Temporal Dead Zone(TDZ)
+/*
+    HOISTING : make some variables accessable before they are initialized
+             - code is scanned for variable declaration before execution
+    
+    Let and Const variables are TDZ(Temporal Dead Zone)
+*/
+
+// Hoistins and TDZ
+
+// Variables
+console.log(me);        // returns undefined
+// console.log(year);   // referenceerror
+// console.log(job);    // referenceerror   
+var me = "Tejas";
+let job = "Programmer";
+const year = 2005;
+
+// Functions
+console.log(addDecl(1, 2))
+// console.log(addExpr(1, 2))
+// console.log(addArr(1, 2))
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+var addArr = (a, b) => a + b;
+
+/*
+- We can only use function declarations before init
+- var/let/const can be called before init
+*/
+
+// Example
+// Don't do this because variables with var are undefined
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log("All Products deleted");
+}
+
+// see window in console
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
