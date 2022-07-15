668b599f95d852b3d12a91dad4f5d7820df46b19
Tejas Naik <RNTejas2005@gmail.com>
2022-07-15 15:46:45 +0530

    Scoping in JavaScript

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -13,3 +13,35 @@ event loop
 JavaScript Engine is a program that executes JavaScript Code
 
 */
+
+// Scope and Scope Chain
+/*
+Scoping controls how our program variables are organized and accessed.
+- Where do the variables live?
+
+Lexical Scoping
+- Scoping controlled by placement of functions and blocks in code; 
+- Nested functions have access to the parent functions variables.
+- global variables are accessed by everywhere.
+*/
+
+// Scoping in practice
+function calcAge(birthYear) {
+    const age = 2022 - birthYear;
+    // console.log(firstName);
+
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+        if (birthYear >= 1981 && birthYear <= 1991) {
+            const str = `Oh, you are a millenial ${firstName}`
+            console.log(str)
+        }
+    }
+
+    printAge();
+    return age;
+}
+
+const firstName = "Tejas";
+calcAge(1986);
