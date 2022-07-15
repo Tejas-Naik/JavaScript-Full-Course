5cca919a003fb972fc7fef21f951de4fb7277c6f
Tejas Naik <RNTejas2005@gmail.com>
2022-07-15 14:29:42 +0530

    Javascript engine & runtime

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -3,8 +3,13 @@
 // Overview of JavaScript
 /*
 JavaScript is a High Level, Garbage-collected, Interpreted or just-in-time
-compiled, Multi Paradigm, Prototype based Object oriented programming languge 
-with First Class Function, Dynamically Typed, single threaded non-blocking 
+compiled, Multi Paradigm, Prototype based Object oriented programming languge
+with First Class Function, Dynamically Typed, single threaded non-blocking
 event loop
 */
 
+// JavaScript Engine and Runtime
+/*
+JavaScript Engine is a program that executes JavaScript Code
+
+*/
