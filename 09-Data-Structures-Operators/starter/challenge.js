32414e88f14f89eeb62fd988252a82f49cafaec5
Tejas Naik <RNTejas2005@gmail.com>
2022-07-24 11:30:42 +0530

    Challenge 2 complete

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -109,3 +109,68 @@ console.log(winner)
 
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
 printGoals(...game.scored)
+
+/*
+Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names �
+
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+GOOD LUCK �
+*/
+
+for (const [goal, shooter] of game.scored.entries()) {
+    console.log(`Goal ${goal + 1} : ${shooter}`);
+};
+
+let sum = 0;
+let length = 0;
+for (const value of Object.values(game.odds)) {
+    sum += value;
+    length++
+}
+console.log(`Avarage is ${sum / length}`)
+
+// Odd of victory Bayern Munich: 1.33
+console.log(`Odd of victory ${game.team1}: ${game.odds.team1}`);
+console.log(`Odd of draw : ${game.odds.x}`);
+console.log(`Odd of victory ${game.team2}: ${game.odds.team2}`);
+
+// const scorers = {
+//     'Lewandowski': 1,
+//     'Gnarby': 1,
+//     'Lewandowski': 1,
+//     'Hummels': 1,
+// }
+
+const scorers = {}
+// for o in obj
+
+for (let x of game.scored) {
+    scorers[x]++ || (scorers[x] = 1);
+}
+// check if o in obj
+// if not add with the value it has
+console.log(scorers);
