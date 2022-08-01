82e4954fff7d702b36cd94950635188f54a0034a
Tejas Naik <RNTejas2005@gmail.com>
2022-08-01 16:27:25 +0530

    Completed Section 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -760,4 +760,14 @@ const planesInLine = function (n) {
 
 planesInLine(10);
 
+// String Method Practice
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  console.log(`${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll("_", " ")} from ${from.slice(0, 3).toUpperCase()} to ${to.slice(0, 3).toUpperCase()} (${time})`.padStart(50, " "));
+
+}
 
