dd21f6939ce5e0507c86bc9c23d2d9b619202771
Tejas Naik <RNTejas2005@gmail.com>
2022-08-01 13:40:21 +0530

    Completed Chellenge 4

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -234,3 +234,69 @@ for (const [time, event] of gameEvents) {
     console.log(`${half} ${time} : ${event}`);
 
 }
+
+/*
+
+Coding Challenge #4
+
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+    const text = document.querySelector('textarea').value;
+    const rows = text.split("\n");
+    console.log(rows);
+
+    for (const row of rows) {
+        const [first, second] = row.toLowerCase().trim().split("_");
+        const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+        console.log(`${output.padEnd(25, " ")}${"✅".repeat(rows.indexOf(row) + 1)}`);
+    }
+})
+
+
+/*
+// solution
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+    const text = document.querySelector('textarea').value.toLowerCase();
+    const array = text.split('\n');
+    const trimed = [];
+
+    for (const a of array) {
+        const b = a.trim();
+        //const names = b.split('_');
+
+        trimed.push(b);
+    }
+
+    for (let i = 0; i < trimed.length; i++) {
+        const a = trimed[i].split('_');
+        const b = a[0] + a[0 + 1][0].toUpperCase() + a[1].slice(1);
+        console.log(`${b.padEnd(20, ' ')}${'✅'.repeat(i + 1)}`);
+    }
+});
+*/
