94c84c724d8bdf94e1d40dc830637c161ae8590b
Tejas Naik <RNTejas2005@gmail.com>
2022-08-02 19:32:58 +0530

    HIGHER ORDER FUNCTIONS

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -27,7 +27,7 @@ createBooking("LF121", 12);
 createBooking("LF121", undefined, 12);  // default passangers
 
 */
-
+/*
 ///////////////////////////////////////
 // How Passing Arguments Works: Value vs. Reference
 ///////////////////////////////////////
@@ -65,5 +65,44 @@ newPassport(tejas);
 console.log(tejas.passport);
 checkIn(flightNum, tejas)
 
+*/
 
+///////////////////////////////////////
+// FIRST CLASS /HIGHER ORDER FUNCTIONS
+///////////////////////////////////////
+// functions are first class citizens that means they are simply values because functions are another type of object and objects are treated as values so vice verse
+// Higher order functions are the functions that recieves a function as arguement or returns a function or both
+// btnClose.addEventListener('click', greet)
+//          higher order func        callback
 
+const oneWord = str => str.replaceAll(" ", "").toLowerCase();
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(" ");
+    return [first.toUpperCase(), ...others].join(" ")
+};
+
+// HIGHER ORDER FUNCTIONS - Functions accepting callback functions
+const transformer = function (str, fn) {
+    console.log(`Original string : ${str}`);
+    console.log(`Transformed string : ${fn(str)}`);
+    console.log(`Transformed by : ${fn.name}`);
+}
+
+transformer("here we are where we were", upperFirstWord);
+transformer("here we are where we were", oneWord);
+
+// Functions returning functions - HIGHER ORDER FUNCTIONS
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+greet("Hey")("Tejas");
+const greeter = greet("Hii");
+greeter("Tejas");
+greeter("Jonas");
+
+const greetArrow = greet => name => console.log(`${greet} ${name}`);
+greetArrow("Hii")("Arrow")
