5567015d3c4a6e3e404c29557e364d48131c35f7
Tejas Naik <RNTejas2005@gmail.com>
2022-08-05 19:46:11 +0530

    Completed Challlenge 2 in Functions

--- 10-Functions/starter/challenge.js
+++ 10-Functions/starter/challenge.js
@@ -65,5 +65,33 @@ const poll = {
 document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
 
 
-poll.showResults.call({ answers: [5, 3, 2] })
-poll.showResults.call({ answers: [5, 3, 2, 9, 9] })
+poll.showResults.call({ answers: [5, 3, 2] });
+poll.showResults.call({ answers: [5, 3, 2, 9, 9] });
+
+/*
+Coding Challenge #2
+This is more of a thinking challenge than a coding challenge �
+Your tasks:
+
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+
+2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example.
+    (function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    })();
+GOOD LUCK �
+*/
+let changeColor;
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    document.body.addEventListener('click', function () {
+        header.style.color = "blue";
+    })
+})();
+
