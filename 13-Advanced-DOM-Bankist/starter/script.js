109d22bf505b286b6b5cc1e0c081f162738283a7
Tejas Naik <RNTejas2005@gmail.com>
2022-10-12 21:21:19 +0530

    Lazy Image Loading

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -195,6 +195,35 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 })
+
+// Lazy Image Loading
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(
+  loadImg,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "200px",
+  });
+imgTargets.forEach(img => imgObserver.observe(img));
+
 /*
 ////////////////////////////////////
 // LECTURES
