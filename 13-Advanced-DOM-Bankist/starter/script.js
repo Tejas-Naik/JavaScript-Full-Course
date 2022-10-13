49a44e87091597ca4ec33789f8e21ecca2fc7e99
Tejas Naik <RNTejas2005@gmail.com>
2022-10-13 19:32:58 +0530

    Lifecycle DOM event

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -497,3 +497,17 @@ console.log(h1.parentElement.children);
 })
 */
 
+// Lifecycle DOM Event
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree created!", e);
+})
+
+window.addEventListener('load', function (e) {
+  console.log("Page fully loaded", e)
+})
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// })
