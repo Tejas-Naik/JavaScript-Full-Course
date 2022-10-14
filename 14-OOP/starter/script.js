8fb0b88743f49e460ea89ba46dacf590609806da
Tejas Naik <RNTejas2005@gmail.com>
2022-10-14 09:24:35 +0530

    Git Overview and Different types to create classes.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+/*
+What is OOP?
+- OOP is a programming paradigm(style) based on concept of objects.
+- We use ibjects to model (describe) real-world or abstract features.
+- Objects may contain data(properties) and code(methods).
+- in OOP, objects are self-contained pieces/blocks of code.
+- Objects are building blocks of applications, and interact with one another
+- Interactions happen through a public interface (API): methods that the code
+-   outside of the object can access and use to communicate with the object.
+- OOP was developed with goal to maintain the code. for more flexibility.
+
+- Objects that are created in OOP are called Classes.
+- Classes are blueprints for new objects
+- we can create as many objects possible with the bluprint (instances).
+
+How do we design Classes?
+- We can create Classes using these 4 fundamental principles
+    1. Abstraction
+            --> Ignoring and Hiding details that doesnt matter. (High Overview of a function/class).
+    2. Encapsulation
+            --> Keeping some properties private, they cant be accessed by outside.
+    3. Inheritance
+            --> Inheritence is the way of making all properties/methods of a class get inherited by other child classes.
+            --> Creating new classes from the original one
+    4. Polymorphism
+            --> Modifying/changing the inherited class's method
+            --> (Parent[login-method]) --> Child[login-method] - new login method.
+
+*/
