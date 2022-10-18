063d0ce0b6b3a0132f9aa313a7cb357abfcc261f
Tejas Naik <RNTejas2005@gmail.com>
2022-10-18 11:03:18 +0530

    Challenge #3 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -313,6 +313,7 @@ mercedese.accelerate()
 bmw.speedUS = 50;
 console.log(bmw);
 */
+/*
 // Inheritance between "Classes"
 const Person = function (firstName, birthYear) {
     this.firstName = firstName;
@@ -350,3 +351,44 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor)
+*/
+// Coding Challenge #3
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.speed}km/h`);
+}
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.speed}km/h`);
+}
+
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+    console.log(`Battery Charged upto ${this.charge}%`);
+}
+
+EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} is going at ${this.speed}km/h with ${this.charge}`);
+}
+
+const tesla = new EV("Tesla", 140, 23);
+tesla.accelerate();
+tesla.chargeBattery(100);
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+tesla.brake();
