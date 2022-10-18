14c654425aca5d81d15cd2ed566b5655702b5947
Tejas Naik <RNTejas2005@gmail.com>
2022-10-18 11:37:09 +0530

    Inheritance between classes : Object.create()

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -394,6 +394,7 @@ tesla.brake();
 tesla.accelerate();
 tesla.brake();
 */
+/*
 // Inheritance between classes: ES6 Classes
 class PersonCl {
     constructor(fullName, birthYear) {
@@ -447,3 +448,35 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
 martha.introduce();
 martha.calcAge();
+*/
+// Inheritance between classes : Object.create()
+
+const PersonProto = {
+    calcAge() {
+        console.log(2022 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+}
+
+const steven = Object.create(PersonProto);
+steven.init("Steven", 2002)
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+StudentProto.introduce = function () {
+    console.log(`I am ${this.firstName}. I study ${this.course}.`);
+}
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();
+
