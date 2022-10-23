5bc22a0583205e3aebc9f9572c8a597a71cfd593
Tejas-Naik <rntejas2005@gmail.com>
2022-10-23 08:07:32 -0700

    How to plan a web project

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,4 +11,13 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// test
+// How to plan a project
+/*
+1. User Stories - description of the app from the users perspective
+2. Features - What features we want to implement
+3. Flowchart - create a flowchart of all the features and how to build
+4. Architechture - how to implement the flowchart
+from all the above 4 steps you will be done implementing
+
+5. Development.
+*/
