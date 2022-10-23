ca3ff75b6fabf70ff7ba21769223628d8bfb4b71
Tejas-Naik <rntejas2005@gmail.com>
2022-10-23 07:56:05 -0700

    Test

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,3 +11,4 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// test
