c968b13ff4c224f27e16aabbd6ebdacd55f62a1a
Tejas-Naik <rntejas2005@gmail.com>
2022-11-04 23:07:14 -0700

    Asynchronous JavaScript, AJAX and APIs

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+// Asynchronous JavaScript, AJAX & APIs
+- SYNCHRONOUS CODE
+--> Code executed line by line
+--> if a line of code takes 5 sec to execute then the entire script will load after 5 sec
+
+- ASYNCHROOUS CODE
+--> Task runs in the background(timer, events)
+--> Doesn't block the execution
+--> We need callback function to execute after some time, but not all callback functions are async
+
+AJAX
+--> Asynchronous JavaScript And XML
+--> Allow us to communicate with remote web servers and get data
+
+API
+--> Application Programming Interface
+--> API is a way of talking computers with other computers
+--> DOM API, Geolocation API, Own Class API, Online API
+*/
