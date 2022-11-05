24f1fd7a999d256d616764b1a58144ecb4149ed0
Tejas-Naik <rntejas2005@gmail.com>
2022-11-05 09:16:48 -0700

    XMLHttprequest function to make AJAX call

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,9 +1,4 @@
 'use strict';
-
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
-///////////////////////////////////////
 /*
 // Asynchronous JavaScript, AJAX & APIs
 - SYNCHRONOUS CODE
@@ -24,3 +19,45 @@ API
 --> API is a way of talking computers with other computers
 --> DOM API, Geolocation API, Own Class API, Online API
 */
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const countryName = document.querySelector('.country-name');
+
+///////////////////////////////////////
+// AJAX call : XMLHttpRequest
+
+const getCountryData = function (countryName) {
+  const endpoint = `https://restcountries.com/v3.1/name/${countryName}`;
+  const request = new XMLHttpRequest();
+  request.open("GET", endpoint);
+  // const data = request.send(); cant do this because of async js it takes time to make a request so it keeps going down
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Displaying UI
+    countriesContainer.style.opacity = 1;
+    const currentCurrency = Object.values(data.currencies)[0];
+    console.log(currentCurrency);
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.continents[0]}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 10000000).toFixed(2)} Crores</p>
+            <p class="country__row"><span>💰</span>${currentCurrency?.symbol} ${currentCurrency?.name}</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]} </p>
+            
+          </div>
+        </article>
+          `
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+  })
+}
+getCountryData("india");
+getCountryData("usa");
+getCountryData("britain");
