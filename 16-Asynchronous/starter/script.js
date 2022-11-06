0630fa9af98568ff9868cc0b62b05b1b8cc77eba
Tejas-Naik <rntejas2005@gmail.com>
2022-11-06 03:49:28 -0800

    The Callback Hell (you should avoid)

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -26,8 +26,29 @@ const countryName = document.querySelector('.country-name');
 
 ///////////////////////////////////////
 // AJAX call : XMLHttpRequest
+const renderCountry = function (data, className = "") {
+  // Displaying UI
+  const currentCurrency = Object.values(data.currencies)[0];
+  console.log(currentCurrency);
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.continents[0]}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 10000000).toFixed(2)} Crores</p>
+            <p class="country__row"><span>💰</span>${currentCurrency?.symbol} ${currentCurrency?.name}</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]} </p>
+            
+          </div>
+          </article>
+          `
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+}
 
-const getCountryData = function (countryName) {
+const getCountryAndNeighbour = function (countryName) {
+  // AJAX Call country 1
   const endpoint = `https://restcountries.com/v3.1/name/${countryName}`;
   const request = new XMLHttpRequest();
   request.open("GET", endpoint);
@@ -37,27 +58,42 @@ const getCountryData = function (countryName) {
   request.addEventListener("load", function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
+    renderCountry(data);
 
-    // Displaying UI
-    countriesContainer.style.opacity = 1;
-    const currentCurrency = Object.values(data.currencies)[0];
-    console.log(currentCurrency);
-    const html = `
-        <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.continents[0]}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 10000000).toFixed(2)} Crores</p>
-            <p class="country__row"><span>💰</span>${currentCurrency?.symbol} ${currentCurrency?.name}</p>
-            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]} </p>
-            
-          </div>
-        </article>
-          `
-    countriesContainer.insertAdjacentHTML("beforeend", html);
+    // Get neighbour country
+    const neighbour = data.borders?.[0];
+
+    // AJAX Call country 2
+    const endpoint = `https://restcountries.com/v3.1/alpha/${neighbour}`;
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", endpoint);
+    // const data = request.send(); cant do this because of async js it takes time to make a request so it keeps going down
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [dataNeighbour] = JSON.parse(this.responseText);
+      console.log(dataNeighbour);
+      renderCountry(dataNeighbour, "neighbour");
+    })
+
   })
 }
-getCountryData("india");
-getCountryData("usa");
-getCountryData("britain");
+getCountryAndNeighbour("india");
+
+/*
+Callback Hell
+--> calling one AJAX Call inside the other one (nested callbacks)
+
+*/
+setTimeout(() => {
+  console.log("1 Second Passed");
+  setTimeout(() => {
+    console.log("2 Seconds Passed");
+    setTimeout(() => {
+      console.log("3 Seconds Passed");
+      setTimeout(() => {
+        console.log("4 Seconds Passed")
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}, 1000)
