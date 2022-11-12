341fdcdba48a4229b990d386e823d8d2bd15d177
Tejas-Naik <rntejas2005@gmail.com>
2022-11-12 02:43:11 -0800

    Running Promises in parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -385,7 +385,7 @@ const whereAmI = function () {
   }
 
   btn.addEventListener("click", whereAmI);
-*/
+
 // Consuming Promises with Async/Await
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -449,7 +449,7 @@ console.log("1. Will get location");
   }
 })()
 
-// Error Handling with try...catch
+// Error Handling with try... catch
 // How try-catch works in general
 // try {
 //   const x = 1;
@@ -458,3 +458,26 @@ console.log("1. Will get location");
 // } catch (err) {
 //   alert(err);
 // }
+*/
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      // NOTE : Promise.all() will stop executing if one of the promises returns false
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ])
+
+    console.log(data.map(d => d[0].capital));
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+get3Countries("bharat", "china", "japan")
