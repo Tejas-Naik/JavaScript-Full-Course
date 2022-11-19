94ee462434dc48136e721a49e9c20e22f1c5abaf
Tejas-Naik <rntejas2005@gmail.com>
2022-11-19 08:39:37 -0800

    Converting code to modern JS (functional / descriptive)

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,51 +9,63 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// Pure function
+const addExpense = function (state, limits, value, description, user = 'jonas') {
+  const cleanUser = user.toLowerCase();
+
+  return (value <= getLimit(limits, cleanUser)) ? [...state, { value: -value, description, user: cleanUser, }] : state;
+
+};
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(newBudget1, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log(newBudget1);
+console.log(newBudget2);
+
+// const checkExpenses2 = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: "limit" } : entry;
+//   })
+// };
+
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: "limit" } : entry
+  )
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+// Impure function
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    // .map(entry => entry.description)
+    // .slice(-2)
+    // .join(" / ");
+    .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, "")
+  console.log(bigExpenses);
+
+  // let output = '';
+  // for (const entry of budget)
+  //   output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : "";
+
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
-
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
-
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
-  if (value <= getLimit(user)) {
-    budget.push(
-      {
-        value: -value,
-        description,
-        user,
-      });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user))
-      entry.flag = 'limit';
-};
-checkExpenses();
-
-
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : "";
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-console.log(budget);
-logBigExpenses(1000);
+logBigExpenses(finalBudget, 1000);
 
 // Imperative vs Declarative
 // Imperative : How to do things (step by step)
