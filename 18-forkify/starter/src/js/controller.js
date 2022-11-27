6d13cbbce726df7178cb1e8d94ce1e331e41a36a
Tejas-Naik <rntejas2005@gmail.com>
2022-11-27 07:18:48 -0800

    Implementing Pagination - Part 1(the algorithm)

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -7,9 +7,9 @@ import resultsView from './views/resultsView.js';
 import searchView from './views/searchView.js';
 
 // For changing the states automaticaly
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -41,7 +41,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render Search results 
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
 
   } catch (err) {
     console.error(err);
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,12 +1,14 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
     recipe: {},
     search: {
         query: "",
-        results: []
+        results: [],
+        page: 1,
+        resultsPerPage: RESULTS_PER_PAGE,
     }
 };
 
@@ -52,4 +54,13 @@ export const loadSearchResults = async function (query) {
         console.error(`${err} ✌`);
         throw err;
     }
+}
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage;
+    const end = (page * state.search.resultsPerPage);
+
+    return state.search.results.slice(start, end);
 }
